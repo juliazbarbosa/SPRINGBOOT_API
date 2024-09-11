06c4c95013bae0d07f376c0e1efe2df6ff7a1a6d
juliazbarbosa <127155774+juliazbarbosa@users.noreply.github.com>
2024-09-11 19:05:29 -0300

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,11 @@
+INSERT INTO tarefa (titulo, descricao_longa, finalizado, data_prevista_finalizacao, data_finalizacao)
+VALUES ('Planejar trabalho', 'Planejamento do trabalho Springboot Jpa', false, '2024-09-15', null);
+
+INSERT INTO tarefa (titulo, descricao_longa, finalizado, data_prevista_finalizacao, data_finalizacao)
+VALUES ('Criar trabalho', 'Criar trabalho de Springboot com Jpa', false, '2024-10-20', null);
+
+INSERT INTO tarefa (titulo, descricao_longa, finalizado, data_prevista_finalizacao, data_finalizacao)
+VALUES ('Concluir Trabalho', 'Finalizar o desenvolvimento do trabalho e reportar bugs encontrados', true, '2024-05-03', '2024-09-06');
+
+INSERT INTO tarefa (titulo, descricao_longa, finalizado, data_prevista_finalizacao, data_finalizacao)
+VALUES ('Enviar tarbalho', 'O projeto final deve ser entregue no GITHUB em um repositório publico', false, '2024-08-22', null);
