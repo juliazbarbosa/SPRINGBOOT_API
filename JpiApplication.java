1d152e0f3c1ee96ddc97b3239c3b6f6a201439d9
juliazbarbosa <127155774+juliazbarbosa@users.noreply.github.com>
2024-09-11 18:53:12 -0300

    Create JpiApplication.java

---
+++ JpiApplication.java
@@ -0,0 +1,13 @@
+package br.edu.univille.poo.JPA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarefaJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpiApplication.class, args);
+	}
+
+}
